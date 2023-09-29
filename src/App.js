373b76d277c238750fd01e72f33603e6056a0f00
Nama' Salameh <nama.salameh@gmail.com>
2023-09-29 23:31:01 +0300

    Added TrendsForYou component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import LeftSideBar from './components/LeftSideBar/LeftSideBar.jsx'
 import MightLike from './components/MightLike/MightLike.jsx';
+import TrendsForYou from './components/TrensForYou/TrendsForYou.jsx';
 function App() {
   return (
     <div className="App">
       <LeftSideBar/>
       <MightLike/>
+      <TrendsForYou/>
     </div>
   );
 }
